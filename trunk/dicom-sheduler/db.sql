1bf93b08e7f271a95f9f2ad49ecb0b03a78f7a33
derenok <derenok@0910c928-f3b2-11de-a89a-43a960051dd3>
2010-01-07 10:23:11 +0000

    Incomplete - task 1: [Sheduler] необходимо разработать. http://code.google.com/p/web-dicom/issues/detail?id=1

--- trunk/dicom-sheduler/db.sql
+++ trunk/dicom-sheduler/db.sql
@@ -1,7 +1,8 @@
 
 CREATE TABLE bigdog.products (
-	itemNumber INT NOT NULL,
-	price DECIMAL(5, 2),
-	stockDate DATE,
-	description VARCHAR(128)
+	/*itemNumber INT NOT NULL,*/
+	/*price DECIMAL(5, 2),*/
+	birthDate DATE,
+	PatientName VARCHAR(128)
+	DicomFile VARCHAR(128)
 ) 
