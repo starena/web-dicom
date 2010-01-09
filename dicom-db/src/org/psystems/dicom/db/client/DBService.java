b4c258044f76aa244b59fea95ecda11db38731d2
derenok <derenok@0910c928-f3b2-11de-a89a-43a960051dd3>
2010-01-09 18:28:08 +0000

    Incomplete - task 2: [Storage] необходимо разработать. http://code.google.com/p/web-dicom/issues/detail?id=2

---
+++ dicom-db/src/org/psystems/dicom/db/client/DBService.java
@@ -0,0 +1,16 @@
+package org.psystems.dicom.db.client;
+
+import com.google.gwt.user.client.rpc.RemoteService;
+import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
+
+/**
+ * The client side stub for the RPC service.
+ */
+@RemoteServiceRelativePath("db")
+public interface DBService extends RemoteService {
+	String greetServer(String name);
+
+	String startDB();
+
+	String stopDB();
+}
