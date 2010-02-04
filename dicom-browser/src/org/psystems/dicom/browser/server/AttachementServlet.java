76d94c0c1f20f91c027f90ba0b0d3fe371fb6f92
derenok <derenok@0910c928-f3b2-11de-a89a-43a960051dd3>
2010-02-04 06:10:02 +0000

    Вся конфигурация получается из конфига
    Incomplete - task 3: [Browser] необходимо разработать. ","Type-Enhancement, Priority-Medium 
    http://code.google.com/p/web-dicom/issues/detail?id=3

--- dicom-browser/src/org/psystems/dicom/browser/server/AttachementServlet.java
+++ dicom-browser/src/org/psystems/dicom/browser/server/AttachementServlet.java
@@ -10,83 +10,63 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.sql.DataSource;
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
 
 public class AttachementServlet extends HttpServlet {
 
 	private static Logger logger = Logger.getLogger(AttachementServlet.class
 			.getName());
-	static {
-//		PropertyConfigurator.configure("WEB-INF/log4j.properties");
-	}// TODO Убрать !!!
-	// FIXME Взять imagesRootDir из конфига
-	private String imagesRootDir = "C:\\WORK\\workspace\\dicom-sheduler\\test\\testdata\\out";
 
-	// private static final Logger log =
-	// Logger.getLogger(AttachementServlet.class
-	// .getName());
+	static {
+		// PropertyConfigurator.configure("WEB-INF/log4j.properties");
+	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 
-//		String teamColor = getServletConfig().getInitParameter("teamColor");
-//		System.out.println("!!! teamColor "+teamColor);
-		
-		
-		
-//		System.out.println("!!! prop "+System.getProperty("myapp.notify-url"));
-//		System.out.println("!!! end "+System.getenv("DEFAULT_ENCODING_DDV"));
+		// String teamColor = getServletConfig().getInitParameter("teamColor");
+		// System.out.println("!!! teamColor "+teamColor);
 
-		
+		// System.out.println("!!! prop "+System.getProperty("myapp.notify-url"));
+		// System.out.println("!!! end "+System.getenv("DEFAULT_ENCODING_DDV"));
+
 		String path = req.getPathInfo().replaceFirst("/", "");
 		String fileName = null;
 		resp.setCharacterEncoding("utf-8");
 		resp.setContentType("image/jpeg");// По умолчанию
-		
-		//Смотрим, если передан Integer, зачит ищем по ID
+
+		String imagesRootDir = getServletContext().getInitParameter(
+				"webdicom.dir.dst");
+
+		// Смотрим, если передан Integer, зачит ищем по ID
 		int imageId = 0;
 		try {
 			imageId = Integer.valueOf(path);
-		}catch(NumberFormatException ex) {
+		} catch (NumberFormatException ex) {
 			fileName = imagesRootDir + File.separator + path;
 		}
 
 		PreparedStatement psSelect = null;
 		try {
-			// FIXME Сделать получение соединения через pool
-//			if (Util.connection == null)
-//				Util.getConnection();
-			
-			
-		    
-//		    InitialContext ic = new InitialContext();
-//		    DataSource myDS = (DataSource)ic.lookup("java:comp/env/jdbc/webdicom");
-//		    Connection connection = myDS.getConnection();
-
 
 			Connection connection = Util.getConnection(getServletContext());
 
 			if (fileName == null) {
-				//ищем по ID
+				// ищем по ID
 				psSelect = connection
-				.prepareStatement("SELECT ID, CONTENT_TYPE, IMAGE_FILE_NAME "
-						+ " FROM WEBDICOM.IMAGES WHERE ID = ? ");
+						.prepareStatement("SELECT ID, CONTENT_TYPE, IMAGE_FILE_NAME "
+								+ " FROM WEBDICOM.IMAGES WHERE ID = ? ");
 				psSelect.setInt(1, imageId);
 			} else {
 				psSelect = connection
-					.prepareStatement("SELECT ID, CONTENT_TYPE, IMAGE_FILE_NAME "
-							+ " FROM WEBDICOM.IMAGES WHERE IMAGE_FILE_NAME = ? ");
+						.prepareStatement("SELECT ID, CONTENT_TYPE, IMAGE_FILE_NAME "
+								+ " FROM WEBDICOM.IMAGES WHERE IMAGE_FILE_NAME = ? ");
 				psSelect.setString(1, path);
 			}
 			ResultSet rs = psSelect.executeQuery();
@@ -100,15 +80,14 @@ public class AttachementServlet extends HttpServlet {
 				index++;
 				break;
 			}
-			if(index == 0) {
+			if (index == 0) {
 				resp.setCharacterEncoding("utf-8");// FIXME Не работает!!!
-				resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Image no found! id=" + imageId + " file=" 
-						+ fileName );
+				resp.sendError(HttpServletResponse.SC_NOT_FOUND,
+						"Image no found! id=" + imageId + " file=" + fileName);
 				return;
 			}
 
-		} 
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			logger.error(e);
 			e.printStackTrace();
 		} finally {
