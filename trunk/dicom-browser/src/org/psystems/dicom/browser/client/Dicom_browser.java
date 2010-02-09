42e218bf0bb8807b0a902e7df4c7c6dfff647809
derenok <derenok@0910c928-f3b2-11de-a89a-43a960051dd3>
2010-02-09 10:51:08 +0000

    Рефакторинг
    Incomplete - task 3: [Browser] необходимо разработать. ","Type-Enhancement, Priority-Medium 
    http://code.google.com/p/web-dicom/issues/detail?id=3

--- trunk/dicom-browser/src/org/psystems/dicom/browser/client/Dicom_browser.java
+++ trunk/dicom-browser/src/org/psystems/dicom/browser/client/Dicom_browser.java
@@ -1,27 +1,19 @@
 package org.psystems.dicom.browser.client;
 
-import java.util.Iterator;
-
 import org.psystems.dicom.browser.client.proxy.DcmFileProxy;
-import org.psystems.dicom.browser.client.proxy.DcmImageProxy;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.logical.shared.CloseEvent;
-import com.google.gwt.event.logical.shared.CloseHandler;
 import com.google.gwt.event.logical.shared.SelectionEvent;
 import com.google.gwt.event.logical.shared.SelectionHandler;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HorizontalPanel;
-import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
-import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.SuggestBox;
 import com.google.gwt.user.client.ui.SuggestOracle;
@@ -33,26 +25,14 @@ import com.google.gwt.user.client.ui.SuggestOracle.Suggestion;
  */
 public class Dicom_browser implements EntryPoint {
 
-	private DialogBox errorDialogBox;
-	private HTML errorResponseLabel;
-
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
-
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting
-	 * service.
-	 */
+	// Create a remote service proxy
 	private final BrowserServiceAsync browserService = GWT
 			.create(BrowserService.class);
+
+	private DialogBox errorDialogBox;
+	private HTML errorResponseLabel;
 	private Button sendButton;
 	private SuggestBox nameField;
-	protected String datePattern = "dd.MM.yyyy";
 
 	/**
 	 * This is the entry point method.
