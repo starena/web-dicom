54217e4b5fa7d9485eb1aa130874a5638f386c4a
derenok <derenok@0910c928-f3b2-11de-a89a-43a960051dd3>
2010-02-10 12:31:42 +0000

    доработка статистики
    Incomplete - task 1: [Sheduler] необходимо разработать.","Type-Enhancement, Priority-Medium, Component-Logic, Component-UI 
    http://code.google.com/p/web-dicom/issues/detail?id=1

---
+++ dicom-browser/src/org/psystems/dicom/browser/server/StatDailyLoadChartServlet.java
@@ -0,0 +1,168 @@
+package org.psystems.dicom.browser.server;
+
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.text.DateFormat;
+import java.util.Calendar;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import sun.security.jca.GetInstance;
+
+public class StatDailyLoadChartServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	
+	private static Logger logger = Logger.getLogger(StatDailyLoadChartServlet.class);
+
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws IOException {
+
+		response.setContentType("image/png");
+
+		OutputStream outputStream = response.getOutputStream();
+
+		CategoryDataset dataset = createDataset();
+		JFreeChart chart = getChart(dataset);
+		int width = 700;
+		int height = 450;
+		ChartUtilities.writeChartAsPNG(outputStream, chart, width, height);
+
+	}
+
+	public JFreeChart getChart(CategoryDataset dataset) {
+		 // create the chart...
+        final JFreeChart chart = ChartFactory.createBarChart(
+            "Динамика загрузки даннных",         // chart title
+            "Дата",               // domain axis label
+            "Значение",                  // range axis label
+            dataset,                  // data
+            PlotOrientation.VERTICAL, // orientation
+            true,                     // include legend
+            true,                     // tooltips?
+            false                     // URLs?
+        );
+
+        // NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
+
+        // set the background color for the chart...
+        chart.setBackgroundPaint(Color.white);
+
+        // get a reference to the plot for further customisation...
+        final CategoryPlot plot = chart.getCategoryPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+        plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+
+        // set the range axis to display integers only...
+        final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+
+        // disable bar outlines...
+        final BarRenderer renderer = (BarRenderer) plot.getRenderer();
+        renderer.setDrawBarOutline(false);
+        
+        // set up gradient paints for series...
+        final GradientPaint gp0 = new GradientPaint(
+            0.0f, 0.0f, Color.blue, 
+            0.0f, 0.0f, Color.lightGray
+        );
+        final GradientPaint gp1 = new GradientPaint(
+            0.0f, 0.0f, Color.green, 
+            0.0f, 0.0f, Color.lightGray
+        );
+        final GradientPaint gp2 = new GradientPaint(
+            0.0f, 0.0f, Color.red, 
+            0.0f, 0.0f, Color.lightGray
+        );
+        renderer.setSeriesPaint(0, gp0);
+        renderer.setSeriesPaint(1, gp1);
+        renderer.setSeriesPaint(2, gp2);
+
+        final CategoryAxis domainAxis = plot.getDomainAxis();
+        domainAxis.setCategoryLabelPositions(
+            CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 6.0)
+        );
+        // OPTIONAL CUSTOMISATION COMPLETED.
+        
+        return chart;
+	}
+	
+	/**
+     * Returns a sample dataset.
+     * 
+     * @return The dataset.
+     */
+    private CategoryDataset createDataset() {
+        
+        // row keys...
+        final String series1 = "Исследования (DCM-файлы)";
+        final String series2 = "Изображения (JPG-файлы)";
+        final String series3 = "Общее";
+
+        Calendar calendar = Calendar.getInstance();
+        
+        int tzoffset = calendar.getTimeZone().getOffset(calendar.getTimeInMillis());
+		System.out.println("!!!!! "+tzoffset );
+        
+        long time = calendar.getTimeInMillis();
+		time = time - (time % (60 * 60 * 24 * 1000))- tzoffset;
+		calendar.setTimeInMillis(time);
+		
+        for(int i=0; i<5; i++) {
+        	calendar.roll(Calendar.DAY_OF_YEAR, false);
+        	System.out.println("!!! " + calendar.getTime());
+        	
+        }
+
+        
+        // column keys...
+        final String category1 = "Category 1";
+        final String category2 = "Category 2";
+        final String category3 = "Category 3";
+        final String category4 = "Category 4";
+        final String category5 = "Category 5";
+
+        // create the dataset...
+        final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        dataset.addValue(1.0, series1, category1);
+        dataset.addValue(4.0, series1, category2);
+        dataset.addValue(3.0, series1, category3);
+        dataset.addValue(5.0, series1, category4);
+        dataset.addValue(5.0, series1, category5);
+
+        dataset.addValue(5.0, series2, category1);
+        dataset.addValue(7.0, series2, category2);
+        dataset.addValue(6.0, series2, category3);
+        dataset.addValue(8.0, series2, category4);
+        dataset.addValue(4.0, series2, category5);
+
+        dataset.addValue(4.0, series3, category1);
+        dataset.addValue(3.0, series3, category2);
+        dataset.addValue(2.0, series3, category3);
+        dataset.addValue(3.0, series3, category4);
+        dataset.addValue(6.0, series3, category5);
+        
+        return dataset;
+        
+    }
+
+}
