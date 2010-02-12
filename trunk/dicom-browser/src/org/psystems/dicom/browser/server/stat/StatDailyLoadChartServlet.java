aed6359f41f4b5aa20b149d20130e2530d06d5ed
derenok <derenok@0910c928-f3b2-11de-a89a-43a960051dd3>
2010-02-12 07:33:55 +0000

    доработка статистики
    Incomplete - task 3: [Browser] необходимо разработать. ","Type-Enhancement, Priority-Medium 
    http://code.google.com/p/web-dicom/issues/detail?id=3

--- trunk/dicom-browser/src/org/psystems/dicom/browser/server/stat/StatDailyLoadChartServlet.java
+++ trunk/dicom-browser/src/org/psystems/dicom/browser/server/stat/StatDailyLoadChartServlet.java
@@ -1,10 +1,16 @@
 package org.psystems.dicom.browser.server.stat;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.GradientPaint;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.text.DateFormat;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import javax.servlet.http.HttpServlet;
@@ -19,18 +25,22 @@ import org.jfree.chart.axis.CategoryAxis;
 import org.jfree.chart.axis.CategoryLabelPositions;
 import org.jfree.chart.axis.NumberAxis;
 import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.IntervalMarker;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.chart.renderer.category.BarRenderer;
 import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
-
-import sun.security.jca.GetInstance;
+import org.jfree.ui.Layer;
+import org.jfree.ui.RectangleAnchor;
+import org.jfree.ui.TextAnchor;
+import org.psystems.dicom.browser.server.Util;
 
 public class StatDailyLoadChartServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
-	
-	private static Logger logger = Logger.getLogger(StatDailyLoadChartServlet.class);
+
+	private static Logger logger = Logger
+			.getLogger(StatDailyLoadChartServlet.class);
 
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws IOException {
@@ -48,121 +58,175 @@ public class StatDailyLoadChartServlet extends HttpServlet {
 	}
 
 	public JFreeChart getChart(CategoryDataset dataset) {
-		 // create the chart...
-        final JFreeChart chart = ChartFactory.createBarChart(
-            "Динамика загрузки даннных",         // chart title
-            "Дата",               // domain axis label
-            "Значение",                  // range axis label
-            dataset,                  // data
-            PlotOrientation.VERTICAL, // orientation
-            true,                     // include legend
-            true,                     // tooltips?
-            false                     // URLs?
-        );
+		// create the chart...
+		final JFreeChart chart = ChartFactory.createBarChart(
+				"Динамика загрузки даннных", // chart title
+				"Дата", // domain axis label
+				"Значение", // range axis label
+				dataset, // data
+				PlotOrientation.VERTICAL, // orientation
+				true, // include legend
+				true, // tooltips?
+				false // URLs?
+				);
 
-        // NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
+		// NOW DO SOME OPTIONAL CUSTOMISATION OF THE CHART...
 
-        // set the background color for the chart...
-        chart.setBackgroundPaint(Color.white);
+		// set the background color for the chart...
+		chart.setBackgroundPaint(Color.white);
 
-        // get a reference to the plot for further customisation...
-        final CategoryPlot plot = chart.getCategoryPlot();
-        plot.setBackgroundPaint(Color.lightGray);
-        plot.setDomainGridlinePaint(Color.white);
-        plot.setRangeGridlinePaint(Color.white);
+		// get a reference to the plot for further customisation...
+		final CategoryPlot plot = chart.getCategoryPlot();
+		plot.setBackgroundPaint(Color.lightGray);
+		plot.setDomainGridlinePaint(Color.white);
+		plot.setRangeGridlinePaint(Color.white);
 
-        // set the range axis to display integers only...
-        final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
-        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+		final IntervalMarker target = new IntervalMarker(4.5, 7.5);
+		target.setLabel("Ожидаемый диапазон");
+		target.setLabelFont(new Font("SansSerif", Font.ITALIC, 11));
+		target.setLabelAnchor(RectangleAnchor.LEFT);
+		target.setLabelTextAnchor(TextAnchor.CENTER_LEFT);
+		target.setPaint(new Color(222, 222, 255, 128));
+		plot.addRangeMarker(target, Layer.BACKGROUND);
 
-        // disable bar outlines...
-        final BarRenderer renderer = (BarRenderer) plot.getRenderer();
-        renderer.setDrawBarOutline(false);
-        
-        // set up gradient paints for series...
-        final GradientPaint gp0 = new GradientPaint(
-            0.0f, 0.0f, Color.blue, 
-            0.0f, 0.0f, Color.lightGray
-        );
-        final GradientPaint gp1 = new GradientPaint(
-            0.0f, 0.0f, Color.green, 
-            0.0f, 0.0f, Color.lightGray
-        );
-        final GradientPaint gp2 = new GradientPaint(
-            0.0f, 0.0f, Color.red, 
-            0.0f, 0.0f, Color.lightGray
-        );
-        renderer.setSeriesPaint(0, gp0);
-        renderer.setSeriesPaint(1, gp1);
-        renderer.setSeriesPaint(2, gp2);
+		// set the range axis to display integers only...
+		final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+		rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
 
-        final CategoryAxis domainAxis = plot.getDomainAxis();
-        domainAxis.setCategoryLabelPositions(
-            CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 6.0)
-        );
-        // OPTIONAL CUSTOMISATION COMPLETED.
-        
-        return chart;
+		// disable bar outlines...
+		final BarRenderer renderer = (BarRenderer) plot.getRenderer();
+		renderer.setDrawBarOutline(false);
+		renderer.setItemMargin(0.10);
+
+		// set up gradient paints for series...
+		final GradientPaint gp0 = new GradientPaint(0.0f, 0.0f, Color.blue,
+				0.0f, 0.0f, Color.lightGray);
+		final GradientPaint gp1 = new GradientPaint(0.0f, 0.0f, Color.green,
+				0.0f, 0.0f, Color.lightGray);
+		final GradientPaint gp2 = new GradientPaint(0.0f, 0.0f, Color.red,
+				0.0f, 0.0f, Color.lightGray);
+		// renderer.setSeriesPaint(0, gp0);
+		// renderer.setSeriesPaint(1, gp1);
+		// renderer.setSeriesPaint(2, gp2);
+
+		final CategoryAxis domainAxis = plot.getDomainAxis();
+		domainAxis.setCategoryLabelPositions(CategoryLabelPositions
+				.createUpRotationLabelPositions(0)
+		// CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 6.0)
+				);
+		// OPTIONAL CUSTOMISATION COMPLETED.
+
+		return chart;
 	}
-	
+
 	/**
-     * Returns a sample dataset.
-     * 
-     * @return The dataset.
-     */
-    private CategoryDataset createDataset() {
-        
-        // row keys...
-        final String series1 = "Исследования (DCM-файлы)";
-        final String series2 = "Изображения (JPG-файлы)";
-        final String series3 = "Общее";
+	 * Returns a sample dataset.
+	 * 
+	 * @return The dataset.
+	 */
+	private CategoryDataset createDataset() {
 
-        Calendar calendar = Calendar.getInstance();
-        
-        int tzoffset = calendar.getTimeZone().getOffset(calendar.getTimeInMillis());
-		System.out.println("!!!!! "+tzoffset );
-        
-        long time = calendar.getTimeInMillis();
-		time = time - (time % (60 * 60 * 24 * 1000))- tzoffset;
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+		// row keys...
+		String series1 = "Исследования (DCM-файлы)";
+		String series2 = "Изображения (JPG-файлы)";
+		String series3 = "Общее";
+
+		Calendar calendar = Calendar.getInstance();
+
+		int tzoffset = calendar.getTimeZone().getOffset(
+				calendar.getTimeInMillis());
+		// System.out.println("!!!!! "+tzoffset );
+
+		long time = calendar.getTimeInMillis();
+		time = time - (time % (60 * 60 * 24 * 1000)) - tzoffset;
 		calendar.setTimeInMillis(time);
-		
-        for(int i=0; i<5; i++) {
-        	calendar.roll(Calendar.DAY_OF_YEAR, false);
-        	System.out.println("!!! " + calendar.getTime());
-        	
-        }
 
-        
-        // column keys...
-        final String category1 = "Category 1";
-        final String category2 = "Category 2";
-        final String category3 = "Category 3";
-        final String category4 = "Category 4";
-        final String category5 = "Category 5";
+		SimpleDateFormat format = new SimpleDateFormat("dd.MM.yyyy");
 
-        // create the dataset...
-        final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+		PreparedStatement psSelect = null;
 
-        dataset.addValue(1.0, series1, category1);
-        dataset.addValue(4.0, series1, category2);
-        dataset.addValue(3.0, series1, category3);
-        dataset.addValue(5.0, series1, category4);
-        dataset.addValue(5.0, series1, category5);
+		try {
+			Connection connection = Util.getConnection(getServletContext());
+			psSelect = connection
+					.prepareStatement("SELECT METRIC_VALUE_LONG, METRIC_DATE"
+							+ " FROM WEBDICOM.DAYSTAT WHERE METRIC_NAME = ? and " +
+									" METRIC_DATE BETWEEN ? AND ? ");
+			
+			Calendar calendar1 = (Calendar) calendar.clone();
+			calendar1.add(Calendar.DAY_OF_MONTH, - 7);
+			
 
-        dataset.addValue(5.0, series2, category1);
-        dataset.addValue(7.0, series2, category2);
-        dataset.addValue(6.0, series2, category3);
-        dataset.addValue(8.0, series2, category4);
-        dataset.addValue(4.0, series2, category5);
+				
+				String dateStr = format.format(calendar.getTime());
+				 System.out.println("!!! " + dateStr);
+				
+				psSelect.setString(1, "ALL_DCM_SIZE");//ALL_DCM_SIZE ALL_IMAGE_SIZE CLIENT_CONNECTIONS
+				psSelect.setDate(2, new java.sql.Date(calendar1.getTimeInMillis()));
+				psSelect.setDate(3, new java.sql.Date(calendar.getTimeInMillis()));
+				ResultSet rs = psSelect.executeQuery();
+				
+				while (rs.next()) {
+					long value = rs.getLong("METRIC_VALUE_LONG");
+					Date date = rs.getDate("METRIC_DATE");
+					dateStr = format.format(date.getTime());
+					String category = dateStr;
+					dataset.addValue(value, series1, category);
+					System.out.println("!!!! ALL_DCM_SIZE="+dateStr+"="+value);
+				}
+				rs.close();
+				
+				psSelect.setString(1, "ALL_IMAGE_SIZE");//ALL_DCM_SIZE ALL_IMAGE_SIZE CLIENT_CONNECTIONS
+				psSelect.setDate(2, new java.sql.Date(calendar1.getTimeInMillis()));
+				psSelect.setDate(3, new java.sql.Date(calendar.getTimeInMillis()));
+				rs = psSelect.executeQuery();
+				
+				while (rs.next()) {
+					long value = rs.getLong("METRIC_VALUE_LONG");
+					Date date = rs.getDate("METRIC_DATE");
+					dateStr = format.format(date.getTime());
+					String category = dateStr;
+					dataset.addValue(value, series2, category);
+					System.out.println("!!!! ALL_IMAGE_SIZE= "+dateStr+"="+value);
+				}
+				rs.close();
+				
+//				psSelect.setString(1, "CLIENT_CONNECTIONS");//ALL_DCM_SIZE ALL_IMAGE_SIZE CLIENT_CONNECTIONS
+//				psSelect.setDate(2, new java.sql.Date(calendar1.getTimeInMillis()));
+//				psSelect.setDate(3, new java.sql.Date(calendar.getTimeInMillis()));
+//				rs = psSelect.executeQuery();
+//				
+//				while (rs.next()) {
+//					long value = rs.getLong("METRIC_VALUE_LONG");
+//					Date date = rs.getDate("METRIC_DATE");
+//					dateStr = format.format(date.getTime());
+//					String category = dateStr;
+//					dataset.addValue(value, series3, category);
+//					System.out.println("!!!! CLIENT_CONNECTIONS= "+dateStr+"="+value);
+//				}
+//				rs.close();
+				
+//				dataset.addValue(Math.random() * 100, series1, category);
+//				dataset.addValue(Math.random() * 100, series2, category);
+//				dataset.addValue(Math.random() * 100, series3, category);
 
-        dataset.addValue(4.0, series3, category1);
-        dataset.addValue(3.0, series3, category2);
-        dataset.addValue(2.0, series3, category3);
-        dataset.addValue(3.0, series3, category4);
-        dataset.addValue(6.0, series3, category5);
-        
-        return dataset;
-        
-    }
+
+		} catch (SQLException e) {
+			logger.error(e);
+			e.printStackTrace();
+		} finally {
+
+			try {
+				if (psSelect != null)
+					psSelect.close();
+			} catch (SQLException e) {
+				logger.error(e);
+			}
+		}
+
+		return dataset;
+
+	}
 
 }
