fe6edeaeb6eb97d9707db22eb2f5638fc7112f1e
derenok <derenok@0910c928-f3b2-11de-a89a-43a960051dd3>
2010-03-16 05:37:28 +0000

    Добавлен Copyright в исходный код

--- trunk/dicom-archive/src/org/psystems/dicom/daemon/Archive.java
+++ trunk/dicom-archive/src/org/psystems/dicom/daemon/Archive.java
@@ -1,41 +1,57 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is part of dcm4che, an implementation of DICOM(TM) in
- * Java(TM), hosted at http://sourceforge.net/projects/dcm4che.
- *
- * The Initial Developer of the Original Code is
- * Gunter Zeilinger, Huetteldorferstr. 24/10, 1150 Vienna/Austria/Europe.
- * Portions created by the Initial Developer are Copyright (C) 2002-2005
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- * Gunter Zeilinger <gunterze@gmail.com>
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+/*
+    WEB-DICOM - preserving and providing information to the DICOM devices
+	
+    Copyright (C) 2009-2010 psystems.org
+    Copyright (C) 2009-2010 Dmitry Derenok 
 
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>
+    
+    The Original Code is part of WEB-DICOM, an implementation hosted at 
+    <http://code.google.com/p/web-dicom/>
+    
+    In the project WEB-DICOM used the library open source project dcm4che
+    The Original Code is part of dcm4che, an implementation of DICOM(TM) in
+    Java(TM), hosted at http://sourceforge.net/projects/dcm4che.
+    
+    =======================================================================
+    
+    WEB-DICOM - Сохранение и предоставление информации с DICOM устройств
+
+    Copyright (C) 2009-2010 psystems.org 
+    Copyright (C) 2009-2010 Dmitry Derenok 
+
+    Это программа является свободным программным обеспечением. Вы можете 
+    распространять и/или модифицировать её согласно условиям Стандартной 
+    Общественной Лицензии GNU, опубликованной Фондом Свободного Программного 
+    Обеспечения, версии 3 или, по Вашему желанию, любой более поздней версии. 
+    Эта программа распространяется в надежде, что она будет полезной, но
+    БЕЗ ВСЯКИХ ГАРАНТИЙ, в том числе подразумеваемых гарантий ТОВАРНОГО СОСТОЯНИЯ ПРИ 
+    ПРОДАЖЕ и ГОДНОСТИ ДЛЯ ОПРЕДЕЛЁННОГО ПРИМЕНЕНИЯ. Смотрите Стандартную 
+    Общественную Лицензию GNU для получения дополнительной информации. 
+    Вы должны были получить копию Стандартной Общественной Лицензии GNU вместе 
+    с программой. В случае её отсутствия, посмотрите <http://www.gnu.org/licenses/>
+    Русский перевод <http://code.google.com/p/gpl3rus/wiki/LatestRelease>
+    
+    Оригинальный исходный код WEB-DICOM можно получить на
+    <http://code.google.com/p/web-dicom/>
+    
+    В проекте WEB-DICOM использованы библиотеки открытого проекта dcm4che/
+    Оригинальный исходный код проекта dcm4che, и его имплементация DICOM(TM) in
+    Java(TM), находится здесь http://sourceforge.net/projects/dcm4che.
+    
+    
+ */
 package org.psystems.dicom.daemon;
 
 import java.awt.image.BufferedImage;
