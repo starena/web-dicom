ab67f5ec87c75660ae550d3fec990ba89caa3cdd
derenok <derenok@0910c928-f3b2-11de-a89a-43a960051dd3>
2010-03-23 10:28:20 +0000

    Сделал рабочий поиск

--- dicom-index/src/com/asutp/index/Indexator.java
+++ dicom-index/src/com/asutp/index/Indexator.java
@@ -24,6 +24,12 @@ import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.Fieldable;
 import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.SimpleFSDirectory;
 import org.apache.lucene.util.Attribute;
 import org.apache.lucene.util.AttributeImpl;
@@ -40,41 +46,46 @@ public class Indexator {
 
 	private String inputDir = "testdata/in";
 	private String outDir = "testdata/out";
+	private String fieldName = "content";
 
 	public Indexator() {
-		
-		
-		
+
 		try {
-//			analyze();
-			make();
+			 analyze();
+			// make();
+//			search("водная таблица работы артезианских скважин НГДУ");
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+//		} catch (ParseException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 
 	private void analyze() throws IOException {
 		// TODO Auto-generated method stub
-//		WhitespaceAnalyzer analyzer = new WhitespaceAnalyzer();
-//		StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_CURRENT);
+		// WhitespaceAnalyzer analyzer = new WhitespaceAnalyzer();
+		// StandardAnalyzer analyzer = new
+		// StandardAnalyzer(Version.LUCENE_CURRENT);
 		SXMAnalyzer analyzer = new SXMAnalyzer();
-		
-		String text = "Y==305&&X==203&&HEIGHT ==15&&WIDTH ==45&&LAYER==!SOIVidget_LAYER_0.30252851845360773&&PARAM==1100016,3,4010000,5,Давление НК [БН ЦДНГ-6 - (Северо-Юрьевское) 4 ГЗУ4],2,&&VTYPE==SOIVidget_INFO&&PARENT==0&&GNAME==!SOIVidget_INFO_0.5889943553914814&&^^";
-		TokenStream stream = analyzer.tokenStream("content", new StringReader(text));
-		System.out.println("Use tokinizer: "+stream.getClass());
-		for(Iterator<Class<? extends Attribute>> iter = stream.getAttributeClassesIterator(); iter.hasNext();) {
+
+		String text = "Y==305&&X==203&&HEIGHT ==15&&WIDTH ==45&&LAYER==!SOIVidget_LAYER_0.30252851845360773&&PARAM==1100016,3,4010000,5,Давление НК [БН ЦДНГ-6 - (Северо-Юрьевское) 4 ГЗУ4],2,&&VTYPE==SOIVidget_INFO&&PARENT==0&&GNAME==!SOIVidget_INFO_0.5889943553914814&&^^" +
+				"Y==665&&X==31&&NOTHIDING==1&&WIDTH==45&&LAYER==!SOIVidget_LAYER_0.18855396644095967&&ALIGN==Center&&HINT==КНС №1 Северо-Юрьевского м/р&&MTYPE==GotoUrl&&PARENT==0&&VTYPE==SOIVidget_MISC&&GNAME==!SOIVidget_MISC_0.9362122612031901&&DESCR==КНС 1&&URL==../../webclient.cgi?file=/common/kns/6_kns-1su.sxm&&^^";
+		TokenStream stream = analyzer.tokenStream("content", new StringReader(
+				text));
+		System.out.println("Use tokinizer: " + stream.getClass());
+		for (Iterator<Class<? extends Attribute>> iter = stream
+				.getAttributeClassesIterator(); iter.hasNext();) {
 			Class<? extends Attribute> att = iter.next();
-			System.out.println("att="+att);
-		} 
-		
-		while(stream.incrementToken()) {
-		TermAttribute termAtt = stream.getAttribute(TermAttribute.class);
-		System.out.print("<" +termAtt.term()+">");
+			System.out.println("att=" + att);
 		}
 
-		
-		
+		while (stream.incrementToken()) {
+			TermAttribute termAtt = stream.getAttribute(TermAttribute.class);
+			System.out.print("<" + termAtt.term() + ">");
+		}
 
 	}
 
@@ -82,8 +93,8 @@ public class Indexator {
 		File index = new File(outDir);
 		SimpleFSDirectory indexdir = new SimpleFSDirectory(index);
 		SXMAnalyzer analyzer = new SXMAnalyzer();
-//		StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_CURRENT);
-		
+		// StandardAnalyzer analyzer = new
+		// StandardAnalyzer(Version.LUCENE_CURRENT);
 
 		IndexWriter writer = new IndexWriter(indexdir, analyzer,
 				IndexWriter.MaxFieldLength.LIMITED);
@@ -106,8 +117,9 @@ public class Indexator {
 		File[] files = dir.listFiles();
 		for (int i = 0; i < files.length; i++) {
 
-			if(files[i].getName().equals(".svn")) continue;
-			
+			if (files[i].getName().equals(".svn"))
+				continue;
+
 			BufferedReader br = new BufferedReader(new InputStreamReader(
 					new FileInputStream(files[i]), "Cp1251"));
 			String inpStr = null;
@@ -117,12 +129,38 @@ public class Indexator {
 			}
 			br.close();
 			Document doc = new Document();
-			doc.add(new Field("content", sb.toString(), Field.Store.YES,
+			doc.add(new Field(fieldName, sb.toString(), Field.Store.YES,
 					Field.Index.ANALYZED));
 			writer.addDocument(doc);
 
 		}
 	}
 
-	
+	private void search(String text) throws IOException, ParseException {
+
+		System.out.println("searching query text = " + text);
+		
+		File index = new File(outDir);
+		SimpleFSDirectory indexdir = new SimpleFSDirectory(index);
+		SXMAnalyzer analyzer = new SXMAnalyzer();
+
+		// Now search the index:
+		IndexSearcher isearcher = new IndexSearcher(indexdir, true); // read-only=true
+		// Parse a simple query that searches for "text":
+		QueryParser parser = new QueryParser(Version.LUCENE_CURRENT, fieldName,
+				analyzer);
+		Query query = parser.parse(text);
+		ScoreDoc[] hits = isearcher.search(query, null, 1000).scoreDocs;
+
+		// assertEquals(1, hits.length);
+		System.out.println("hits.length=" + hits.length);
+		// Iterate through the results:
+		for (int i = 0; i < hits.length; i++) {
+			Document hitDoc = isearcher.doc(hits[i].doc);
+			System.out.println("Find: "
+					+ hitDoc.get(fieldName));
+		}
+		isearcher.close();
+	}
+
 }
