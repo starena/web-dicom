012ce5de034a730ed4b65827d2ce035ee0dc5695
derenok <derenok@0910c928-f3b2-11de-a89a-43a960051dd3>
2010-03-24 11:08:32 +0000

    Доработка индексатора

--- dicom-index/src/com/asutp/index/Indexator.java
+++ dicom-index/src/com/asutp/index/Indexator.java
@@ -46,22 +46,30 @@ public class Indexator {
 
 	private String inputDir = "testdata/in";
 	private String outDir = "testdata/out";
-	private String fieldName = "content";
+	private String fieldContentName = "content";
+	private String fieldSXMName = "sxm";
+	
+	Analyzer analyzer;
 
 	public Indexator() {
 
+//		analyzer = new SXMAnalyzer();
+		analyzer = new StandardAnalyzer(Version.LUCENE_CURRENT);
+		
 		try {
-			 analyze();
-			// make();
-//			search("водная таблица работы артезианских скважин НГДУ");
+//			 analyze();
+			 make();
+			try {
+//				search("\"Сводная таблица работы артезианских скважин НГДУ \"Нижнесортымснефть\"\"");
+				search("ЦДНГ-6");
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-//		} catch (ParseException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 	}
 
 	private void analyze() throws IOException {
@@ -69,9 +77,9 @@ public class Indexator {
 		// WhitespaceAnalyzer analyzer = new WhitespaceAnalyzer();
 		// StandardAnalyzer analyzer = new
 		// StandardAnalyzer(Version.LUCENE_CURRENT);
-		SXMAnalyzer analyzer = new SXMAnalyzer();
+		
 
-		String text = "Y==305&&X==203&&HEIGHT ==15&&WIDTH ==45&&LAYER==!SOIVidget_LAYER_0.30252851845360773&&PARAM==1100016,3,4010000,5,Давление НК [БН ЦДНГ-6 - (Северо-Юрьевское) 4 ГЗУ4],2,&&VTYPE==SOIVidget_INFO&&PARENT==0&&GNAME==!SOIVidget_INFO_0.5889943553914814&&^^" +
+		String text = "Y==305&&X==203&&HEIGHT ==15&&WIDTH ==45&&LAYER==!SOIVidget_LAYER_0.30252851845360773&&PARAM==1100016,3,4010000,5,Давление НК [БН ЦДНГ-6 - (Северо-Юрьевское) 4 ГЗУ4],2,&&VTYPE==SOIVidget_INFO&&PARENT==0&&GNAME==!SOIVidget_INFO_0.5889943553914814&&^^\r\n" +
 				"Y==665&&X==31&&NOTHIDING==1&&WIDTH==45&&LAYER==!SOIVidget_LAYER_0.18855396644095967&&ALIGN==Center&&HINT==КНС №1 Северо-Юрьевского м/р&&MTYPE==GotoUrl&&PARENT==0&&VTYPE==SOIVidget_MISC&&GNAME==!SOIVidget_MISC_0.9362122612031901&&DESCR==КНС 1&&URL==../../webclient.cgi?file=/common/kns/6_kns-1su.sxm&&^^";
 		TokenStream stream = analyzer.tokenStream("content", new StringReader(
 				text));
@@ -92,7 +100,6 @@ public class Indexator {
 	private void make() throws IOException {
 		File index = new File(outDir);
 		SimpleFSDirectory indexdir = new SimpleFSDirectory(index);
-		SXMAnalyzer analyzer = new SXMAnalyzer();
 		// StandardAnalyzer analyzer = new
 		// StandardAnalyzer(Version.LUCENE_CURRENT);
 
@@ -120,6 +127,7 @@ public class Indexator {
 			if (files[i].getName().equals(".svn"))
 				continue;
 
+			String filename = files[i].getName();
 			BufferedReader br = new BufferedReader(new InputStreamReader(
 					new FileInputStream(files[i]), "Cp1251"));
 			String inpStr = null;
@@ -129,7 +137,9 @@ public class Indexator {
 			}
 			br.close();
 			Document doc = new Document();
-			doc.add(new Field(fieldName, sb.toString(), Field.Store.YES,
+			doc.add(new Field(fieldContentName, sb.toString(), Field.Store.NO,
+					Field.Index.ANALYZED));
+			doc.add(new Field(fieldSXMName, filename, Field.Store.YES,
 					Field.Index.ANALYZED));
 			writer.addDocument(doc);
 
@@ -142,12 +152,12 @@ public class Indexator {
 		
 		File index = new File(outDir);
 		SimpleFSDirectory indexdir = new SimpleFSDirectory(index);
-		SXMAnalyzer analyzer = new SXMAnalyzer();
+	
 
 		// Now search the index:
 		IndexSearcher isearcher = new IndexSearcher(indexdir, true); // read-only=true
 		// Parse a simple query that searches for "text":
-		QueryParser parser = new QueryParser(Version.LUCENE_CURRENT, fieldName,
+		QueryParser parser = new QueryParser(Version.LUCENE_CURRENT, fieldContentName,
 				analyzer);
 		Query query = parser.parse(text);
 		ScoreDoc[] hits = isearcher.search(query, null, 1000).scoreDocs;
@@ -158,7 +168,7 @@ public class Indexator {
 		for (int i = 0; i < hits.length; i++) {
 			Document hitDoc = isearcher.doc(hits[i].doc);
 			System.out.println("Find: "
-					+ hitDoc.get(fieldName));
+					+ hitDoc.get(fieldSXMName));
 		}
 		isearcher.close();
 	}
