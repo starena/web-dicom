531ad530d22bace531611d99bd0c6c9309d447ad
derenok <derenok@0910c928-f3b2-11de-a89a-43a960051dd3>
2010-09-22 12:23:48 +0000

    Добавил аппараты УЗИ

--- trunk/dicom-browser/src/org/psystems/dicom/browser/client/component/WorkListPanel.java
+++ trunk/dicom-browser/src/org/psystems/dicom/browser/client/component/WorkListPanel.java
@@ -133,6 +133,13 @@ public class WorkListPanel extends Composite {
 		lbManufacturerModelName.addItem("Рентген (РДК 50/6)","РДК 50/6");
 		lbManufacturerModelName.addItem("Рентген (DUODiagnost)","DUODiagnost");
 		
+		lbManufacturerModelName.addItem("УЗИ-1 (SonoScape)","SonoScape-1");
+		lbManufacturerModelName.addItem("УЗИ-2 (SonoScape)","SonoScape-2");
+		lbManufacturerModelName.addItem("УЗИ-3 (SonoScape)","SonoScape-3");
+		
+		lbManufacturerModelName.addItem("УЗИ (VOLUSON)","VOLUSON 730 BT04 EXPERT");
+
+		
 		
 		//TODO Убрал из списка
 //		lbManufacturerModelName.addItem("Мамография","MAMMOGRAF");
