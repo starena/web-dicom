206f3bf36fae6cd70cfcf9449fc35c9661f15add
derenok <derenok@0910c928-f3b2-11de-a89a-43a960051dd3>
2011-04-06 09:00:47 +0000

    Доработан ORM. Сделано сохранение направления. Set issue ID: 73

--- dicom-browser/src/org/psystems/dicom/commons/orm/PersistentManagerDerby.java
+++ dicom-browser/src/org/psystems/dicom/commons/orm/PersistentManagerDerby.java
@@ -51,22 +51,18 @@ public class PersistentManagerDerby implements IPersistentManager {
 
 			try {
 				
-				if (drn.getId() == 0) { // делаем вставку
-					sql = "INSERT ("
+				if (drn.getId() == null) { // делаем вставку
+					sql = "INSERT INTO WEBDICOM.DIRECTION ("
 							+ "DIRECTION_ID," //1
 							+ "DOCTOR_DIRECT_NAME," //2
-							
 							+ "DOCTOR_DIRECT_CODE," //3
-							
 							+ "DIAGNOSIS_DIRECT," //4
 							+ "DATE_DIRECTION," //5
 							+ "SERVICES_DIRECTED," //6
 							+ "DEVICE," //7
 							+ "DIRECTION_DATE_PLANNED," //8
 							+ "DOCTOR_PERFORMED_NAME," //9
-							
 							+ "DOCTOR_PERFORMED_CODE," //10
-							
 							+ "DIRECTION_CODE," //11
 							+ "DIRECTION_LOCATION," //12
 							+ "DIAGNOSIS_PERFORMED," //13
@@ -78,39 +74,124 @@ public class PersistentManagerDerby implements IPersistentManager {
 							+ "PATIENT_SEX," //19
 							+ "DATE_MODIFIED," //20
 							+ "REMOVED" //21
-							+") INTO WEBDICOM.DIRECTION VALUES"
-							+ "(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+							+") VALUES "
+							+ "(?,?,?,?,?,?,?,?,?,?," +
+								"?,?,?,?,?,?,?,?,?,?,?)";
 					
 				pstmt = connection.prepareStatement(sql);
 				pstmt.setString(1, drn.getDirectionId());
-				pstmt.setString(2, drn.getDoctorDirect().getEmployeName());
-				pstmt.setString(3, drn.getDoctorDirect().getEmployeCode());
-				pstmt.setString(4, Diagnosis.toPersistentCollectionString(drn.getDiagnosisDirect()));
-				pstmt.setDate(5, java.sql.Date.valueOf(formatSQL.format(drn.getDateDirection())) );
-				pstmt.setString(6, Service.toPersistentCollectionString(drn.getServicesDirect()));
-				//TODO device Может сделать доп. поля ^...^ как в услугах?
-				pstmt.setString(7, drn.getDevice().getManufacturerModelName());
-				pstmt.setDate(8, java.sql.Date.valueOf(formatSQL.format(drn.getDatePlanned())) );
-				pstmt.setString(9, drn.getDoctorPerformed().getEmployeName());
-				pstmt.setString(10, drn.getDoctorPerformed().getEmployeCode());
+				
+				if (drn.getDoctorDirect() != null)
+					pstmt.setString(2, drn.getDoctorDirect().getEmployeName());
+				else
+					pstmt.setNull(2, java.sql.Types.VARCHAR);
+
+				if (drn.getDoctorDirect() != null)
+					pstmt.setString(3, drn.getDoctorDirect().getEmployeCode());
+				else
+					pstmt.setNull(3, java.sql.Types.VARCHAR);
+
+				if (drn.getDiagnosisDirect() != null)
+					pstmt.setString(4, Diagnosis
+							.toPersistentCollectionString(drn
+									.getDiagnosisDirect()));
+				else
+					pstmt.setNull(4, java.sql.Types.VARCHAR);
+
+				if (drn.getDateDirection() != null)
+					pstmt.setDate(5, java.sql.Date.valueOf(formatSQL.format(drn
+							.getDateDirection())));
+				else
+					pstmt.setNull(5, java.sql.Types.DATE);
+
+				if (drn.getServicesDirect() != null)
+					pstmt.setString(6, Service.toPersistentCollectionString(drn
+							.getServicesDirect()));
+				else
+					pstmt.setNull(6, java.sql.Types.VARCHAR);
+
+				// TODO device Может сделать доп. поля ^...^ как в услугах?
+				if (drn.getDevice() != null)
+					pstmt.setString(7, drn.getDevice()
+							.getManufacturerModelName());
+				else
+					pstmt.setNull(7, java.sql.Types.VARCHAR);
+
+				if (drn.getDatePlanned() != null)
+					pstmt.setDate(8, java.sql.Date.valueOf(formatSQL.format(drn
+							.getDatePlanned())));
+				else
+					pstmt.setNull(8, java.sql.Types.DATE);
+
+				if (drn.getDoctorPerformed() != null) {
+					pstmt.setString(9, drn.getDoctorPerformed()
+							.getEmployeName());
+					pstmt.setString(10, drn.getDoctorPerformed()
+							.getEmployeCode());
+				} else {
+					pstmt.setNull(9, java.sql.Types.VARCHAR);
+					pstmt.setNull(10, java.sql.Types.VARCHAR);
+				}
+
 				pstmt.setString(11, drn.getDirectionCode());
 				pstmt.setString(12, drn.getDirectionLocation());
-				pstmt.setString(13, Diagnosis.toPersistentCollectionString(drn.getDiagnosisPerformed()));
-				pstmt.setString(14, Service.toPersistentCollectionString(drn.getServicesPerformed()));
-				pstmt.setDate(15, java.sql.Date.valueOf(formatSQL.format(drn.getDatePerformed())) );
-				pstmt.setString(16, drn.getPatient().getPatientId());
-				pstmt.setString(17, drn.getPatient().getPatientName());
-				pstmt.setDate(18, java.sql.Date.valueOf(formatSQL.format(drn.getPatient().getPatientBirthDate())) );
-				pstmt.setString(19, drn.getPatient().getPatientSex());
-				pstmt.setDate(20, java.sql.Date.valueOf(formatSQL.format(new java.util.Date())));// sysdate
-				pstmt.setDate(18, java.sql.Date.valueOf(formatSQL.format(drn.getDateRemoved())));
+
+				if (drn.getDiagnosisPerformed() != null)
+					pstmt.setString(13, Diagnosis
+							.toPersistentCollectionString(drn
+									.getDiagnosisPerformed()));
+				else
+					pstmt.setNull(13, java.sql.Types.VARCHAR);
+
+				if (drn.getServicesPerformed() != null)
+					pstmt.setString(14, Service
+							.toPersistentCollectionString(drn
+									.getServicesPerformed()));
+				else
+					pstmt.setNull(14, java.sql.Types.VARCHAR);
+
+				if (drn.getDatePerformed() != null)
+					pstmt.setDate(15, java.sql.Date.valueOf(formatSQL
+							.format(drn.getDatePerformed())));
+				else
+					pstmt.setNull(15, java.sql.Types.DATE);
+
+				if (drn.getPatient() != null) {
+					pstmt.setString(16, drn.getPatient().getPatientId());
+					pstmt.setString(17, drn.getPatient().getPatientName());
+					pstmt.setString(19, drn.getPatient().getPatientSex());
+				} else {
+					pstmt.setNull(16, java.sql.Types.VARCHAR);
+					pstmt.setNull(17, java.sql.Types.VARCHAR);
+					pstmt.setNull(19, java.sql.Types.VARCHAR);
+				}
+				
+				if (drn.getPatient() != null && drn.getPatient().getPatientBirthDate() != null) {
+					pstmt.setDate(18, java.sql.Date
+							.valueOf(formatSQL.format(drn.getPatient()
+									.getPatientBirthDate())));
+				} else {
+					pstmt.setNull(18, java.sql.Types.DATE);
+				}
+
+				pstmt.setDate(20, java.sql.Date.valueOf(formatSQL
+						.format(new java.util.Date())));// sysdate
+
+				if (drn.getDateRemoved() != null)
+					pstmt.setDate(21, java.sql.Date.valueOf(formatSQL
+							.format(drn.getDateRemoved())));
+				else 
+					pstmt.setNull(21, java.sql.Types.DATE);
 				
 				connection.setAutoCommit(false);
 				int count = pstmt.executeUpdate();
-				ResultSet keys = pstmt.getGeneratedKeys();
-				keys.next();
-				resultId = keys.getLong("ID");
 				connection.commit();
+				
+				//TODO Не работает!!!
+//				ResultSet keys = pstmt.getGeneratedKeys();
+//				keys.next();
+//				resultId = keys.getLong("ID");
+				
 			} else {
 				//TODO Сделать корректную проверку на оюновление направления в БД.
 				//по каким полям. по ID? или directionId?...
---
+++ dicom-browser/src/org/psystems/dicom/commons/orm/PersistentManagerDerbyTest.java
@@ -0,0 +1,121 @@
+package org.psystems.dicom.commons.orm;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Properties;
+
+import junit.framework.TestCase;
+
+public class PersistentManagerDerbyTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testMakePesistent() {
+
+		try {
+			Properties props = new Properties(); // connection properties
+			props.put("user", "user1"); // FIXME взять из конфига
+			props.put("password", "user1"); // FIXME взять из конфига
+			Connection connection;
+
+			connection = DriverManager.getConnection(
+					"jdbc:derby://localhost:1527//DICOM/DB/WEBDICOM"
+							+ ";create=true", props);
+
+			PersistentManagerDerby pm = new PersistentManagerDerby(connection);
+
+			Direction drn = new Direction();
+			drn.setDateDirection(new Date());
+
+			ManufacturerDevice device = new ManufacturerDevice();
+			device.setManufacturerModelName("TestModel");
+			device.setManufacturerModelDescription("Тестовый аппарат");
+			device.setManufacturerModelType("ES");
+			device.setManufacturerModelTypeDescription("Эндоскоп");
+			drn.setDevice(device);
+
+			ArrayList<Diagnosis> diagnosisDirect = new ArrayList<Diagnosis>();
+
+			Diagnosis dia = new Diagnosis();
+			dia.setDiagnosisCode("M01.1");
+			dia.setDiagnosisType(Diagnosis.TYPE_MAIN);// основной
+			dia.setDiagnosisDescription("Заболевание такое-то...");
+			dia.setDiagnosisSubType("Предварительный");
+			diagnosisDirect.add(dia);
+
+			dia = new Diagnosis();
+			dia.setDiagnosisCode("K01.1");
+			dia.setDiagnosisType(Diagnosis.TYPE_ACCOMPANYING);// сопутствующий
+			dia.setDiagnosisDescription("Еще одно заболевание такое-то...");
+			dia.setDiagnosisSubType("Предварительный");
+			diagnosisDirect.add(dia);
+
+			drn.setDiagnosisDirect(diagnosisDirect);
+
+			drn.setDirectionCode("Test code");
+			drn.setDirectionId("123456");
+			drn.setDirectionLocation("605");
+
+			Employe doctorDirect = new Employe();
+			doctorDirect.setEmployeCode("123");
+			doctorDirect.setEmployeName("Врач Петров И.И.");
+			doctorDirect.setEmployeType(Employe.TYPE_DOCTOR);
+			drn.setDoctorDirect(doctorDirect);
+
+			Patient patient = new Patient();
+			SimpleDateFormat format = new SimpleDateFormat("dd-MM-yyyy");
+
+			try {
+				patient.setPatientBirthDate(format.parse("01-03-1974"));
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			patient.setPatientId("123123");
+			patient.setPatientName("Иванов Иван Иванович");
+			patient.setPatientSex("M");
+			patient.setPatientShortName("ИВАИВ74");
+			drn.setPatient(patient);
+
+			ArrayList<Service> servicesDirect = new ArrayList<Service>();
+			Service srv = new Service();
+			srv.setServiceCode("A.03.16.001.01");
+			srv.setServiceAlias("ЭГДС");
+			srv
+					.setServiceDescription("Эзофагогастродуоденоскопия диагностическая");
+			servicesDirect.add(srv);
+
+			srv = new Service();
+			srv.setServiceCode("A.02.12.002.02");
+			srv.setServiceAlias("СМАД");
+			srv
+					.setServiceDescription("Суточное мониторирование артериального давления");
+			servicesDirect.add(srv);
+
+			drn.setServicesDirect(servicesDirect);
+			
+			try {
+				pm.makePesistent(drn);
+			} catch (DataException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	}
+
+}
