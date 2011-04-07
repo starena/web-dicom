8ac01df907adab11d4975905d0cdad72a28b86c6
derenok <derenok@0910c928-f3b2-11de-a89a-43a960051dd3>
2011-04-07 15:14:49 +0000

    ORM - десериализация и обновление направления. Set issue ID: 73

--- trunk/dicom-browser/src/org/psystems/dicom/commons/orm/PersistentManagerDerbyTest.java
+++ trunk/dicom-browser/src/org/psystems/dicom/commons/orm/PersistentManagerDerbyTest.java
@@ -1,6 +1,5 @@
 package org.psystems.dicom.commons.orm;
 
-import java.io.Serializable;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -16,6 +15,7 @@ public class PersistentManagerDerbyTest extends TestCase {
 
 	protected void setUp() throws Exception {
 		super.setUp();
+		//TODO Сделать старт тестовой НЕСЕТЕВОЙ инстанции БД
 	}
 
 	protected void tearDown() throws Exception {
@@ -25,6 +25,11 @@ public class PersistentManagerDerbyTest extends TestCase {
 	public void testMakePesistent() {
 
 		try {
+			
+			//TODO Сделать старт тестовой НЕСЕТЕВОЙ инстанции БД
+			
+			//TODO Доделать тест по всем полям и разить тест на несколько функций
+			
 			Properties props = new Properties(); // connection properties
 			props.put("user", "user1"); // FIXME взять из конфига
 			props.put("password", "user1"); // FIXME взять из конфига
@@ -36,7 +41,7 @@ public class PersistentManagerDerbyTest extends TestCase {
 
 			PersistentManagerDerby pm = new PersistentManagerDerby(connection);
 
-			String rndId = ""+Math.random();
+			String rndId = "" + Math.random();
 			Direction drn = new Direction();
 			drn.setDateDirection(new Date());
 
@@ -56,12 +61,12 @@ public class PersistentManagerDerbyTest extends TestCase {
 			dia.setDiagnosisSubType("Предварительный");
 			diagnosisDirect.add(dia);
 
-			dia = new Diagnosis();
-			dia.setDiagnosisCode("K01.1");
-			dia.setDiagnosisType(Diagnosis.TYPE_ACCOMPANYING);// сопутствующий
-			dia.setDiagnosisDescription("Еще одно заболевание такое-то...");
-			dia.setDiagnosisSubType("Предварительный");
-			diagnosisDirect.add(dia);
+			Diagnosis dia1 = new Diagnosis();
+			dia1.setDiagnosisCode("K01.1");
+			dia1.setDiagnosisType(Diagnosis.TYPE_ACCOMPANYING);// сопутствующий
+			dia1.setDiagnosisDescription("Еще одно заболевание такое-то...");
+			dia1.setDiagnosisSubType("Предварительный");
+			diagnosisDirect.add(dia1);
 
 			drn.setDiagnosisDirect(diagnosisDirect);
 
@@ -69,10 +74,10 @@ public class PersistentManagerDerbyTest extends TestCase {
 			drn.setDirectionId(rndId);
 			drn.setDirectionLocation("605");
 
-			Employe doctorDirect = new Employe();
+			Employee doctorDirect = new Employee();
 			doctorDirect.setEmployeCode("123");
 			doctorDirect.setEmployeName("Врач Петров И.И.");
-			doctorDirect.setEmployeType(Employe.TYPE_DOCTOR);
+			doctorDirect.setEmployeType(Employee.TYPE_DOCTOR);
 			drn.setDoctorDirect(doctorDirect);
 
 			Patient patient = new Patient();
@@ -98,20 +103,81 @@ public class PersistentManagerDerbyTest extends TestCase {
 					.setServiceDescription("Эзофагогастродуоденоскопия диагностическая");
 			servicesDirect.add(srv);
 
-			srv = new Service();
-			srv.setServiceCode("A.02.12.002.02");
-			srv.setServiceAlias("СМАД");
-			srv
+			Service srv1 = new Service();
+			srv1.setServiceCode("A.02.12.002.02");
+			srv1.setServiceAlias("СМАД");
+			srv1
 					.setServiceDescription("Суточное мониторирование артериального давления");
-			servicesDirect.add(srv);
+			servicesDirect.add(srv1);
 
 			drn.setServicesDirect(servicesDirect);
 			long newId = pm.makePesistent(drn);
-			
+
+			// **************************
+			// Десериализация
+			// *************************
+
 			Direction newDrn = (Direction) pm.getObjectbyID(newId);
-			assertEquals( rndId, newDrn.getDirectionId());
+			assertEquals(rndId, newDrn.getDirectionId());
+
+			// Diagnosis
+
+			ArrayList<Diagnosis> newDiaList = newDrn.getDiagnosisDirect();
+
+			Diagnosis newDia = newDiaList.get(0);
+			assertEquals(newDia.getDiagnosisCode(), dia.getDiagnosisCode());
+			assertEquals(newDia.getDiagnosisType(), dia.getDiagnosisType());
+			assertEquals(newDia.getDiagnosisSubType(), dia
+					.getDiagnosisSubType());
+			assertEquals(newDia.getDiagnosisDescription(), dia
+					.getDiagnosisDescription());
+
+			Diagnosis newDia1 = newDiaList.get(1);
+			assertEquals(newDia1.getDiagnosisCode(), dia1.getDiagnosisCode());
+			assertEquals(newDia1.getDiagnosisType(), dia1.getDiagnosisType());
+			assertEquals(newDia1.getDiagnosisSubType(), dia1
+					.getDiagnosisSubType());
+			assertEquals(newDia1.getDiagnosisDescription(), dia1
+					.getDiagnosisDescription());
+
+			// Service
+
+			ArrayList<Service> newServicesDirect = newDrn.getServicesDirect();
+
+			Service newSrv = newServicesDirect.get(0);
+			assertEquals(newSrv.getServiceAlias(), srv.getServiceAlias());
+			assertEquals(newSrv.getServiceCode(), srv.getServiceCode());
+			assertEquals(newSrv.getServiceDescription(), srv
+					.getServiceDescription());
+
+			Service newSrv1 = newServicesDirect.get(1);
+			assertEquals(newSrv1.getServiceAlias(), srv1.getServiceAlias());
+			assertEquals(newSrv1.getServiceCode(), srv1.getServiceCode());
+			assertEquals(newSrv1.getServiceDescription(), srv1
+					.getServiceDescription());
 			
 			
+			// Проверка обновления направления
+			
+//			newDia1 = new Diagnosis();
+			newDia1.setDiagnosisCode("K11.2");
+			newDia1.setDiagnosisType(Diagnosis.TYPE_ACCOMPANYING);// сопутствующий
+			newDia1.setDiagnosisDescription("И Еще одно заболевание такое-то...");
+			newDia1.setDiagnosisSubType("Непонятный");
+			
+			pm.makePesistent(newDrn);
+			
+			Direction drn3 = (Direction) pm.getObjectbyID(newDrn.getId());
+			ArrayList<Diagnosis> diaList3 = drn3.getDiagnosisDirect();
+			
+			Diagnosis newDia3 = diaList3.get(1);
+			assertEquals(newDia3.getDiagnosisCode(), newDia1.getDiagnosisCode());
+			assertEquals(newDia3.getDiagnosisType(), newDia1.getDiagnosisType());
+			assertEquals(newDia3.getDiagnosisSubType(), newDia1
+					.getDiagnosisSubType());
+			assertEquals(newDia3.getDiagnosisDescription(), newDia1
+					.getDiagnosisDescription());
+
 
 		} catch (SQLException e1) {
 			// TODO Auto-generated catch block
