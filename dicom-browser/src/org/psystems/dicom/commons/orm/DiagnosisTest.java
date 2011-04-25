99b1b84acb79fa848435749c210e6c8741c51fd9
derenok <derenok@0910c928-f3b2-11de-a89a-43a960051dd3>
2011-04-25 10:59:33 +0000

    Incomplete - task 73: Реализация связки исследования с услугами (через studyType?) http://code.google.com/p/web-dicom/issues/detail?id=73
    make unit test
    Set issue ID: 73

--- dicom-browser/src/org/psystems/dicom/commons/orm/DiagnosisTest.java
+++ dicom-browser/src/org/psystems/dicom/commons/orm/DiagnosisTest.java
@@ -7,11 +7,64 @@ import junit.framework.TestCase;
 public class DiagnosisTest extends TestCase {
 
 	public void testToPersistentString() {
-		fail("Not yet implemented");
+		Diagnosis dia = new Diagnosis();
+		dia.setDiagnosisCode("M01.1");
+		dia.setDiagnosisType(Diagnosis.TYPE_MAIN);
+		dia.setDiagnosisSubType("Предварительный");
+		dia.setDiagnosisDescription("Заболевание M01.1^диагноз");
+		
+		assertEquals(dia.toPersistentString(),
+			"ОСНОВНОЙ^Предварительный^M01.1^Заболевание M01.1#####диагноз");
+		dia.setDiagnosisDescription("Заболевание M01.1|диагноз");
+		
+		assertEquals(dia.toPersistentString(),
+			"ОСНОВНОЙ^Предварительный^M01.1^Заболевание M01.1@@@@@диагноз");
+		
+		dia.setDiagnosisDescription("Заболевание M01.1^|^|диагноз");
+		assertEquals(dia.toPersistentString(),
+			"ОСНОВНОЙ^Предварительный^M01.1^Заболевание M01.1#####@@@@@#####@@@@@диагноз");
+		
+		dia.setDiagnosisCode("M01.1 ^|");
+		dia.setDiagnosisType(Diagnosis.TYPE_MAIN + " ^|");
+		dia.setDiagnosisSubType("Предварительный ^|");
+		dia.setDiagnosisDescription("Заболевание M01.1^диагноз ^|");
+		
+		assertEquals(dia.toPersistentString(),
+		"ОСНОВНОЙ #####@@@@@^Предварительный #####@@@@@^M01.1 #####@@@@@^Заболевание M01.1#####диагноз #####@@@@@");
+		
 	}
 
 	public void testGetFromPersistentString() {
-		Diagnosis.getFromPersistentString("");
+		assertNull(Diagnosis.getFromPersistentString(null));
+		assertNull(Diagnosis.getFromPersistentString(""));
+		
+		Diagnosis dia = Diagnosis.getFromPersistentString("ОСНОВНОЙ^Предварительный^M01.1^Заболевание M01.1");
+		assertEquals(dia.getDiagnosisType(), "ОСНОВНОЙ");
+		assertEquals(dia.getDiagnosisSubType(), "Предварительный");
+		assertEquals(dia.getDiagnosisCode(), "M01.1");
+		assertEquals(dia.getDiagnosisDescription(), "Заболевание M01.1");
+		
+		dia = Diagnosis.getFromPersistentString("ОСНОВНОЙ^Предварительный^M01.1^Описание");
+		assertEquals(dia.getDiagnosisType(), "ОСНОВНОЙ");
+		assertEquals(dia.getDiagnosisSubType(), "Предварительный");
+		assertEquals(dia.getDiagnosisCode(), "M01.1");
+		assertEquals(dia.getDiagnosisDescription(), "Описание");
+		
+		dia = Diagnosis.getFromPersistentString("ОСНОВНОЙ^Предварительный^M01.1^Описание ##### диагноза");
+		assertEquals(dia.getDiagnosisDescription(), "Описание ^ диагноза");
+		
+		dia = Diagnosis.getFromPersistentString("ОСНОВНОЙ^Предварительный^M01.1^Описание ##### @@@@@ диагноза");
+		assertEquals(dia.getDiagnosisDescription(), "Описание ^ | диагноза");
+		
+		try {
+			dia = Diagnosis.getFromPersistentString("ОСНОВНОЙ^Предварительный^M01.1^");
+			fail("Wrong pattern");
+		} catch (IllegalArgumentException ex) {}
+		try {
+			dia = Diagnosis.getFromPersistentString("ОСНОВНОЙ^Предварительный^M01.1^");
+			fail("Wrong pattern");
+		} catch (IllegalArgumentException ex) {}
+		
 	}
 
 	public void testGetCollectionFromPersistentString() {
@@ -21,6 +74,11 @@ public class DiagnosisTest extends TestCase {
 		assertEquals(dias[0].getDiagnosisType(), "ОСНОВНОЙ");
 		assertEquals(dias[0].getDiagnosisSubType(), "Предварительный");
 		assertEquals(dias[0].getDiagnosisDescription(), "Заболевание M01.1");
+		
+		assertEquals(dias[1].getDiagnosisCode(), "K01.1");
+		assertEquals(dias[1].getDiagnosisType(), "СОПУТСТВУЮЩИЙ");
+		assertEquals(dias[1].getDiagnosisSubType(), "Заключительный");
+		assertEquals(dias[1].getDiagnosisDescription(), "Заболевание K01.1");
 	}
 
 	public void testToPersistentCollectionString() {
