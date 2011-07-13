dc074a172dd0f6911f14dc7ba9f74996c4b25505
derenok <derenok@0910c928-f3b2-11de-a89a-43a960051dd3>
2011-07-13 18:24:57 +0000

    Incomplete - task 73: Реализация связки исследования с услугами (через studyType?) http://code.google.com/p/web-dicom/issues/detail?id=73
    Рабочий вариант экспорта в PDF
    Set issue ID: 73

--- dicom-browser/src/org/psystems/dicom/browser/server/ManagePdfServlet.java
+++ dicom-browser/src/org/psystems/dicom/browser/server/ManagePdfServlet.java
@@ -1,12 +1,13 @@
 package org.psystems.dicom.browser.server;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.sql.Connection;
-import java.sql.SQLException;
-import java.util.Enumeration;
+import java.util.Date;
 import java.util.Map;
 import java.util.Set;
 
@@ -16,12 +17,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.psystems.dicom.browser.client.exception.DefaultGWTRPCException;
+import org.psystems.dicom.commons.orm.ORMUtil;
 import org.psystems.dicom.commons.orm.PersistentManagerDerby;
 import org.psystems.dicom.commons.orm.entity.DataException;
 import org.psystems.dicom.commons.orm.entity.Study;
 
-import com.itextpdf.text.BaseColor;
-import com.itextpdf.text.Chunk;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
 import com.itextpdf.text.Font;
@@ -41,7 +42,6 @@ import com.itextpdf.text.pdf.PdfName;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfStamper;
 import com.itextpdf.text.pdf.PushbuttonField;
-import com.itextpdf.text.pdf.TextField;
 import com.itextpdf.text.pdf.AcroFields.Item;
 
 /**
@@ -58,7 +58,33 @@ public class ManagePdfServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-	// req.setCharacterEncoding("cp1251");
+	changePDFContent(req, resp, false);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+	req.setCharacterEncoding("cp1251");
+
+	changePDFContent(req, resp, true);
+    }
+
+    /**
+     * Преообразование PDF-ки
+     * 
+     * @param req
+     * @param resp
+     * @param finalPhase
+     *            - финальная стадия
+     * @throws IOException
+     */
+    private void changePDFContent(HttpServletRequest req, HttpServletResponse resp, boolean finalPhase)
+	    throws IOException {
+
+	String dcmTmpDir = getServletContext().getInitParameter("webdicom.dir.newdcm.tmp");
+	String pdfTmpFilename = dcmTmpDir + "/" + new Date().getTime() + "_" + (int) (Math.random() * 10000000l)
+		+ ".pdf";
+	File pdfTmpFile = null;
 	// TODO Поменять имя переменной webdicom.dir.ootmpl на webdicom.dir.pdf
 	String tmplDir = getServletContext().getInitParameter("webdicom.dir.ootmpl");
 	String file = tmplDir + req.getPathInfo();
@@ -74,24 +100,49 @@ public class ManagePdfServlet extends HttpServlet {
 
 	    FileInputStream fis = new FileInputStream(file);
 	    PdfReader reader = new PdfReader(fis);
-	    OutputStream out = resp.getOutputStream();
-	    PdfStamper stamper = new PdfStamper(reader, out);
+	    pdfTmpFile = new File(pdfTmpFilename);
+	    // OutputStream out = resp.getOutputStream();
+	    // PdfStamper stamper = new PdfStamper(reader, out);
+	    PdfStamper stamper = new PdfStamper(reader, new FileOutputStream(pdfTmpFile));
 
 	    // Пробегаем по полям формы.
 	    // Если поле READ_ONLY - заменяем его на текст
 	    AcroFields form = stamper.getAcroFields();
 
-	    // Аппарата
-	    String fName = "ManufacturerModelName";
+	    String fName;
+
+	    // ФИО Пациента
+	    fName = "PatientName";
+	    if (form.getField(fName) != null)
+		form.setField(fName, study.getPatientName());
+
+	    // ДР Пациента
+	    fName = "PatientBirthDate";
+	    if (form.getField(fName) != null)
+		form.setField(fName, ORMUtil.dateFormatUser.format(ORMUtil.dateFormatSQL.parse(study
+			.getPatientBirthDate())));
+
+	    // Аппарат
+	    fName = "ManufacturerModelName";
 	    if (form.getField(fName) != null)
 		form.setField(fName, study.getManufacturerModelName());
-	    
-	    // Протокол оасотра
+
+	    // Протокол осмотра
 	    fName = "StudyViewprotocol";
-	    if (form.getField(fName) != null) 
+	    if (req.getParameter(fName) != null)
+		study.setStudyViewprotocol(req.getParameter(fName));
+	    if (form.getField(fName) != null)
 		form.setField(fName, study.getStudyViewprotocol());
-	    
-	    
+
+	    // Дата протокола осмотра
+	    fName = "StudyViewprotocolDate";
+	    if (form.getField(fName) != null)
+		form.setField(fName, ORMUtil.dateFormatUser.format(ORMUtil.dateFormatSQL.parse(study
+			.getStudyViewprotocolDate())));
+
+	    // ===================================================================
+	    // TODO !!!======== дополнить остальные поля ==================!!!!!
+	    // ===================================================================
 
 	    for (String fieldName : form.getFields().keySet()) {
 
@@ -108,8 +159,9 @@ public class ManagePdfServlet extends HttpServlet {
 
 		// Свойства поля
 
-//		boolean readOnly = fieldIsREADONLY(form, fieldName);
-//		System.out.println("!!! field " + fieldName + " RO is " + readOnly);
+		// boolean readOnly = fieldIsREADONLY(form, fieldName);
+		// System.out.println("!!! field " + fieldName + " RO is " +
+		// readOnly);
 
 		// ------------
 
@@ -119,31 +171,50 @@ public class ManagePdfServlet extends HttpServlet {
 		// TextField.READ_ONLY, null);
 	    }
 
-	    replaceROFields(reader, stamper);
+	    // Удаляем поля
+	    replaceFields(reader, stamper, !finalPhase);
 
-	    // Добавляем кнопку Submit
-	    int btnWidth = 100;
-	    int btnHeight = 30;
-	    PushbuttonField button = new PushbuttonField(stamper.getWriter(), new Rectangle(90, 60, 90 + btnWidth,
-		    60 + btnHeight), "submit");
-	    button.setText("Передать в архив...");
-	    button.setBackgroundColor(new GrayColor(0.7f));
+	    if (!finalPhase) {
+		// Добавляем кнопку Submit
+		int btnWidth = 100;
+		int btnHeight = 30;
+		PushbuttonField button = new PushbuttonField(stamper.getWriter(), new Rectangle(90, 60, 90 + btnWidth,
+			60 + btnHeight), "submit");
+		button.setText("Передать в архив...");
+		button.setBackgroundColor(new GrayColor(0.7f));
 
-	    // button.setVisibility(PushbuttonField.VISIBLE_BUT_DOES_NOT_PRINT);
+		// button.setVisibility(PushbuttonField.VISIBLE_BUT_DOES_NOT_PRINT);
+		PdfFormField submit = button.getField();
+		submit.setAction(PdfAction.createSubmitForm(req.getServletPath(), null, PdfAction.SUBMIT_HTML_FORMAT));
 
-	    PdfFormField submit = button.getField();
+		String url = req.getServletPath() + req.getPathInfo() + "?" + req.getQueryString();
+		submit.setAction(PdfAction.createSubmitForm(url, null, PdfAction.SUBMIT_HTML_FORMAT));
 
-	    submit.setAction(PdfAction.createSubmitForm(req.getServletPath(), null, PdfAction.SUBMIT_HTML_FORMAT));
-
-	    // submit.setAction(PdfAction.createSubmitForm("http://localhost:8080/template",
-	    // null, PdfAction.SUBMIT_HTML_FORMAT));
-
-	    stamper.addAnnotation(submit, 1);
-
-	    resp.setContentType("application/pdf; charset=UTF-8");
-	    // resp.setCharacterEncoding("UTF-8");
+		stamper.addAnnotation(submit, 1);
+	    }
 
 	    stamper.close();
+	    resp.setContentType("application/pdf; charset=UTF-8");
+
+	    // stamper.close();
+	    // Передача результата в броузер
+	    FileInputStream in = new FileInputStream(pdfTmpFilename);
+	    BufferedOutputStream out = new BufferedOutputStream(resp.getOutputStream());
+
+	    byte b[] = new byte[8];
+	    int count;
+	    while ((count = in.read(b)) != -1) {
+		out.write(b, 0, count);
+
+	    }
+	    out.flush();
+	    out.close();
+	    in.close();
+
+	    // Отправка данных в архив
+	    if (finalPhase)
+		sendToArchive(study, pdfTmpFile);
+
 	} catch (DocumentException e) {
 	    resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
 	    resp.setContentType("text/html; charset=UTF-8");
@@ -169,7 +240,166 @@ public class ManagePdfServlet extends HttpServlet {
 	    String msg = Util.loggingException(e.getMessage(), e);
 	    resp.getWriter().print(msg);
 	    logger.warn(msg);
+	} finally {
+	    if (pdfTmpFile != null)
+		pdfTmpFile.delete();
 	}
+    }
+
+    /**
+     * Отправка в архив PDF-ки
+     * 
+     * @param study
+     * @param pdf
+     * @throws DefaultGWTRPCException
+     * @throws IOException
+     * @throws FileNotFoundException
+     */
+    private void sendToArchive(Study study, File pdf) throws FileNotFoundException, IOException, DefaultGWTRPCException {
+
+	// TODO Коряво все это... переделать. логику сюда перенести.
+
+	java.util.Properties props = new java.util.Properties();
+
+	// TODO Костылек, указвывает тип контента
+	props.put("content_type", "application/pdf");
+
+	// раскручивает теги:
+
+	// Hidden studyInstanceUID = new Hidden();
+	// studyInstanceUID.setName("0020000D");
+	// studyInstanceUID.setValue(proxy.getStudyInstanceUID());
+	// formDataPanel.add(studyInstanceUID);
+	if (study.getStudyInstanceUID() != null)
+	    props.put("0020000D", study.getStudyInstanceUID());
+
+	// Hidden studySeriesUID = new Hidden();
+	// studySeriesUID.setName("0020000E");
+	// studySeriesUID.setValue(proxy.getStudyInstanceUID() + "." + new
+	// Date().getTime());
+	// formDataPanel.add(studySeriesUID);
+	if (study.getStudyInstanceUID() != null)
+	props.put("0020000E", study.getStudyInstanceUID() + "." + new Date().getTime());
+
+	// Hidden studyId = new Hidden();
+	// studyId.setName("00200010");
+	// studyId.setValue(proxy.getStudyId());
+	// formDataPanel.add(studyId);
+	if (study.getStudyId() != null)
+	props.put("00200010", study.getStudyId());
+
+	//
+
+	// Hidden patientId = new Hidden();
+	// patientId.setName("00100021");
+	// patientId.setValue(proxy.getPatientId());
+	// formDataPanel.add(patientId);
+	if (study.getPatientId() != null)
+	props.put("00100021", study.getPatientId());
+
+	// Hidden patientName = new Hidden();
+	// patientName.setName("00100010");
+	// patientName.setValue(proxy.getPatientName());
+	// formDataPanel.add(patientName);
+	if (study.getPatientName() != null)
+	props.put("00100010", study.getPatientName());
+
+	// Hidden patientSex = new Hidden();
+	// patientSex.setName("00100040");
+	// patientSex.setValue(proxy.getPatientSex());
+	// formDataPanel.add(patientSex);
+	if (study.getPatientSex() != null)
+	props.put("00100040", study.getPatientSex());
+
+	// Hidden patientBirthDateHidden = new Hidden();
+	// patientBirthDateHidden.setName("00100030");
+	// patientBirthDateHidden.setValue(proxy.getPatientBirthDate());
+	// formDataPanel.add(patientBirthDateHidden);
+	if (study.getPatientBirthDate() != null)
+	props.put("00100030", study.getPatientBirthDate());
+
+	//
+
+	// Hidden manufacturerModelName = new Hidden();
+	// manufacturerModelName.setName("00081090");
+	// manufacturerModelName.setValue(proxy.getManufacturerModelName());
+	// formDataPanel.add(manufacturerModelName);
+	if (study.getManufacturerModelName() != null)
+	props.put("00081090", study.getManufacturerModelName());
+
+	// Hidden modality = new Hidden();
+	// modality.setName("00080060");
+	// // modality.setValue(proxy.getStudyModality());
+	// modality.setValue(lbModality.getValue(lbModality.getSelectedIndex()));
+	// formDataPanel.add(modality);
+	if (study.getStudyModality() != null)
+	props.put("00080060", study.getStudyModality());
+
+	// Hidden studyDateHidden = new Hidden();
+	// studyDateHidden.setName("00080020");
+	// studyDateHidden.setValue(proxy.getStudyDate());
+	// formDataPanel.add(studyDateHidden);
+	if (study.getStudyDate() != null)
+	props.put("00080020", study.getStudyDate());
+
+	// // Study Completion Date
+	// Hidden studyViewProtocolDateHidden = new Hidden();
+	// studyViewProtocolDateHidden.setName("00321050");
+	// studyViewProtocolDateHidden.setValue(proxy.getStudyViewprotocolDate());
+	// formDataPanel.add(studyViewProtocolDateHidden);
+	if (study.getStudyViewprotocolDate() != null)
+	props.put("00321050", study.getStudyViewprotocolDate());
+
+	// Hidden studyDoctorHidden = new Hidden();
+	// studyDoctorHidden.setName("00080090");
+	// studyDoctorHidden.setValue(proxy.getStudyDoctor());
+	// formDataPanel.add(studyDoctorHidden);
+	if (study.getStudyDoctor() != null)
+	props.put("00080090", study.getStudyDoctor());
+
+	// Hidden studyOperatorHidden = new Hidden();
+	// studyOperatorHidden.setName("00081070");
+	// studyOperatorHidden.setValue(proxy.getStudyOperator());
+	// formDataPanel.add(studyOperatorHidden);
+	if (study.getStudyOperator() != null)
+	props.put("00081070", study.getStudyOperator());
+
+	//
+	// proxy.setStudyDescription(studyDescription.getText());
+	//
+	// Hidden studyDescriptionHidden = new Hidden();
+	// studyDescriptionHidden.setName("00081030");
+	// studyDescriptionHidden.setValue(proxy.getStudyDescription());
+	// formDataPanel.add(studyDescriptionHidden);
+	if (study.getStudyDescription() != null)
+	props.put("00081030", study.getStudyDescription());
+
+	//
+	// proxy.setStudyResult(studyResult.getText());
+	//
+	// Hidden studyResultHidden = new Hidden();
+	// studyResultHidden.setName("00102000");
+	// studyResultHidden.setValue(proxy.getStudyResult());
+	// formDataPanel.add(studyResultHidden);
+	if (study.getStudyResult() != null)
+	props.put("00102000", study.getStudyResult());
+
+	//
+	// proxy.setStudyViewprotocol(studyComments.getText());
+	//
+	// // Tag.StudyComments
+	// Hidden studyCommentsHidden = new Hidden();
+	// studyCommentsHidden.setName("00324000");
+	// studyCommentsHidden.setValue(proxy.getStudyViewprotocol());
+	// formDataPanel.add(studyCommentsHidden);
+	if (study.getStudyViewprotocol() != null)
+	props.put("00324000", study.getStudyViewprotocol());
+
+	//
+
+	FileInputStream fis = new FileInputStream(pdf);
+	org.psystems.dicom.commons.CommonUtil.makeSendDicomFile(getServletContext(), props, fis);
+	fis.close();
     }
 
     /**
@@ -177,10 +407,13 @@ public class ManagePdfServlet extends HttpServlet {
      * 
      * @param reader
      * @param stamper
+     * @param onlyROfields
+     *            удалять только поля которые READ_ONLY
      * @throws IOException
      * @throws DocumentException
      */
-    private void replaceROFields(PdfReader reader, PdfStamper stamper) throws IOException, DocumentException {
+    private void replaceFields(PdfReader reader, PdfStamper stamper, boolean onlyROfields) throws IOException,
+	    DocumentException {
 	Set<String> parameters = stamper.getAcroFields().getFields().keySet();
 	AcroFields form = stamper.getAcroFields();
 
@@ -189,11 +422,10 @@ public class ManagePdfServlet extends HttpServlet {
 	for (String fieldName : fields) {
 
 	    // Замещаем только READ_ONLY поля
-	    if (!fieldIsREADONLY(form, fieldName))
+	    if (onlyROfields && !fieldIsREADONLY(form, fieldName))
 		continue;
 
 	    Item field = form.getFieldItem(fieldName);
-	    System.out.println("!!! replaceFields [" + form.getField(fieldName) + "]");
 	    PdfDictionary widgetDict = field.getWidget(0);
 	    // pdf rectangles are stored as [llx, lly, urx, ury]
 	    PdfArray rectArr = widgetDict.getAsArray(PdfName.RECT); // should
@@ -203,17 +435,12 @@ public class ManagePdfServlet extends HttpServlet {
 	    float urY = rectArr.getAsNumber(3).floatValue();
 
 	    String value = form.getField(fieldName);
-	    System.out.println("!!! replaceFields fieldName=" + fieldName + " " + rectArr + " value:"+value);
-
-	    
-
+	    // Удаляем поле
 	    form.removeField(fieldName);
 
 	    PdfContentByte canvas = stamper.getOverContent(1);
 
-	    // TODO Вынести в конфиг! или положить в приложение
-	    String fontPath = "C:\\WINDOWS\\Fonts\\arial.ttf";
-
+	    String fontPath = "fonts/arial.ttf";
 	    Font font = new Font(BaseFont.createFont(fontPath, "Cp1251", BaseFont.NOT_EMBEDDED), 14);
 
 	    Phrase phrase = new Phrase(value, font);
@@ -257,55 +484,6 @@ public class ManagePdfServlet extends HttpServlet {
 	    }
 	}
 	return false;
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	try {
-
-	    req.setCharacterEncoding("cp1251");
-
-	    String tmplDir = getServletContext().getInitParameter("webdicom.dir.ootmpl");
-	    String file = tmplDir + req.getPathInfo();
-	    System.out.println("! CS = " + req.getCharacterEncoding() + "; " + req.getParameter("charset"));
-
-	    for (Enumeration iter = req.getParameterNames(); iter.hasMoreElements();) {
-		String name = (String) iter.nextElement();
-		System.out.println("!!! name=" + name + "=" + req.getParameter(name));
-	    }
-
-	    FileInputStream fis = new FileInputStream(file);
-	    PdfReader reader = new PdfReader(fis);
-	    OutputStream out = resp.getOutputStream();
-
-	    PdfStamper stamper = new PdfStamper(reader, out);
-
-	    AcroFields fields = stamper.getAcroFields();
-	    Set<String> parameters = fields.getFields().keySet();
-	    for (String parameter : parameters) {
-
-		System.out.println("!!!   parameter=" + parameter + " TYPE=" + fields.getFieldType(parameter) + "["
-			+ req.getParameter(parameter) + "]");
-
-		// пропускаем радиобаттоны
-		if (fields.getFieldType(parameter) == AcroFields.FIELD_TYPE_RADIOBUTTON)
-		    continue;
-
-		if (req.getParameter(parameter) != null) {
-		    fields.setField(parameter, req.getParameter(parameter));
-		}
-		fields.setFieldProperty(parameter, "setfflags", TextField.READ_ONLY, null);
-	    }
-
-	    // stamper.setFormFlattening(true);
-
-	    resp.setContentType("application/pdf");
-	    stamper.close();
-
-	} catch (DocumentException e) {
-	    // TODO Auto-generated catch block
-	    e.printStackTrace();
-	}
     }
 
 }
