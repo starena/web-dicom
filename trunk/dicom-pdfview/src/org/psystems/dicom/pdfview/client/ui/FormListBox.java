b8786348dad70cf72c53ddf0c31360b3f0ecff16
derenok <derenok@0910c928-f3b2-11de-a89a-43a960051dd3>
2012-02-29 12:33:14 +0000

    Доработки по разобру форм.

---
+++ trunk/dicom-pdfview/src/org/psystems/dicom/pdfview/client/ui/FormListBox.java
@@ -0,0 +1,23 @@
+package org.psystems.dicom.pdfview.client.ui;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.ui.ListBox;
+
+public class FormListBox extends ListBox {
+
+	public FormListBox() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public FormListBox(boolean isMultipleSelect) {
+		super(isMultipleSelect);
+		// TODO Auto-generated constructor stub
+	}
+
+	public FormListBox(Element element) {
+		super(element);
+		// TODO Auto-generated constructor stub
+	}
+
+}
