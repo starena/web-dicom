a3b873df74ea8078cd5ee573d9d1e66dbe3c50ab
derenok <derenok@0910c928-f3b2-11de-a89a-43a960051dd3>
2012-03-12 09:50:59 +0000

    Добавил тест для полей

--- dicom-pdfview/src/org/psystems/dicom/pdfview/server/FormField.java
+++ dicom-pdfview/src/org/psystems/dicom/pdfview/server/FormField.java
@@ -2,13 +2,25 @@ package org.psystems.dicom.pdfview.server;
 
 public class FormField implements Comparable<FormField> {
 
-	public String fieldName;
+	private String fieldName;
+	private String fieldTitle;
 	private String fieldNameEncoded;// перекодирован в QUERY_STRING
 	private String value;
-	public float upperRightY;
-	public float upperRightX;
-	public float lowerLeftY;
-	public float lowerLeftX;
+
+	private float upperRightY;
+	private float upperRightX;
+	private float lowerLeftY;
+	private float lowerLeftX;
+
+	/**
+	 * Перечень "автораспознаваемых" тегов: (Operator #00081070)(Doctor
+	 * #00080090)(StudyDate #00080020) (StudyViewprotocolDate #00321050)
+	 * (StudyDescription #00081030) (StudyResult #00102000) (StudyComments
+	 * #00324000)
+	 */
+	private String tag;// DICOM tag
+
+	private String format;// Формат поля (например dd.mm.yyyy для ввода даты)
 
 	/**
 	 * @param name
@@ -17,6 +29,25 @@ public class FormField implements Comparable<FormField> {
 	public FormField(String name) {
 		super();
 		this.fieldName = name;
+		init();
+	}
+
+	/**
+	 * Разбор имени поля (Поле форматированного ввода дата
+	 * DD.MM.YYYY|tag=00030004|format=DD.MM.YYYY)
+	 */
+	public void init() {
+		fieldTitle = fieldName;
+		String[] vals = fieldName.split("\\|");
+		if(vals.length>0) fieldTitle = vals[0];
+		for (String token : vals) {
+			if(token.startsWith("tag=")) {
+				setTag(token.replaceAll("tag\\=", ""));
+			}
+			if(token.startsWith("format=")) {
+				setFormat(token.replaceAll("format\\=", ""));
+			}
+		}
 	}
 
 	public String getFieldName() {
@@ -73,6 +104,30 @@ public class FormField implements Comparable<FormField> {
 
 	public void setValue(String value) {
 		this.value = value;
+	}
+
+	public String getFieldTitle() {
+		return fieldTitle;
+	}
+
+	public void setFieldTitle(String fieldTitle) {
+		this.fieldTitle = fieldTitle;
+	}
+
+	public String getTag() {
+		return tag;
+	}
+
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+
+	public String getFormat() {
+		return format;
+	}
+
+	public void setFormat(String format) {
+		this.format = format;
 	}
 
 	@Override
