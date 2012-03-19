1a3949c99fb1dab031919ecaccfa10ba4df772d3
derenok <derenok@0910c928-f3b2-11de-a89a-43a960051dd3>
2012-03-19 08:38:09 +0000

    Сделал костыль по тикету http://code.google.com/p/web-dicom/issues/detail?id=125

--- dicom-browser/src/org/psystems/dicom/browser/client/component/DirectionCard.java
+++ dicom-browser/src/org/psystems/dicom/browser/client/component/DirectionCard.java
@@ -521,12 +521,13 @@ Resources resources = GWT.create(Resources.class);
 		//
 		proxy.setStudyViewprotocolDate(Utils.dateFormatSql.format(new Date()));
 		
+		String studyInstanceUID = ""+new java.util.Date().getTime()+"."+Math.random();
 		// FIXME Задать инстанс. UID-аппарата (из конфига) + ddmmyyyyhhmmss
 		// Это в том случае если исследование НОВОЕ, если доописываем,
 		// то брать из предудущего изменения (предыдущее изменение того же исследования)
 		// Тут нужно еще разбираться и с SOP Clss UID и прочеее
 		// и о том как оно формируется
-		proxy.setStudyInstanceUID(studyInstanceUID);
+		proxy.setStudyInstanceUID(studyInstanceUID );
 		
 
 	    } else {
